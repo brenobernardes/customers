e1a6bae51acc84ed4c08f440a7686565e5d2a206
Breno Bernardes <breno.pbernardes@gmail.com>
2024-08-01 15:24:51 -0300

    feat: created new  methods

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,28 @@
 class CustomersController < ApplicationController
   def index
+    @customers = Customer.all
   end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def new
+    @customer = Customer.new
+  end
+
+  def create
+    @customer = Customer.new(customer_params)
+
+    if @customer.save
+      redirect_to @customer
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def customer_params
+      params.require(:customer).permit(:first_name, :last_name, :email)
+    end
 end
